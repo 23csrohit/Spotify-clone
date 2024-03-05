07a2bbbf058cbf8fe4c859271befdcc782132e52
23csrohit <149533311+23csrohit@users.noreply.github.com>
2024-03-05 22:51:01 +0530

    First commit

---
+++ js/main.js
@@ -0,0 +1,15 @@
+function toggler(x) {
+    x.classList.toggle("change");
+    var sidenav = document.getElementById("mySidenav");
+    var opacity = document.getElementById("opacity");
+    sidenav.classList.toggle("open");
+    opacity.classList.toggle("opa");
+    document.body.classList.toggle("opa");
+}
+
+function profile() {
+    var profileMenu = document.getElementById("profileMenu");
+    profileMenu.classList.toggle("active");
+    var svg = document.querySelector(".profile-title svg");
+    svg.classList.toggle("transform");
+}
